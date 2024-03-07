ad38c535585bf493c05dbce28dc0b86fafb3ecb2
HudsonGraeme <spencer@inferencelabs.com>
2024-03-07 22:08:41 +0000

    Add SPDX and pragma back into interface

--- contracts/interfaces/IOmronDeposit.sol
+++ contracts/interfaces/IOmronDeposit.sol
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
 /**
  * @title Interface for OmronDeposit Contract
  * @notice This interface outlines the functions and events for the OmronDeposit contract, which allows users to deposit tokens and earn points.
