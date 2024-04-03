a147e2f0fa7a45197f406a34866d4e040711b4b1
HudsonGraeme <spencer@inferencelabs.com>
2024-04-03 21:16:49 +0000

    Update IOmronDeposit interface

--- contracts/interfaces/IOmronDeposit.sol
+++ contracts/interfaces/IOmronDeposit.sol
@@ -7,102 +7,114 @@ pragma solidity 0.8.21;
  * @custom:security-contact whitehat@inferencelabs.com
  */
 interface IOmronDeposit {
+    // Structs
+
     /**
-     * @notice Struct to store user information including token balances and points
-     * @dev UserInfo struct contains mappings for token balances and variables for points tracking
+     * @notice A struct that holds information about a user's points and token balances
      */
     struct UserInfo {
-        mapping(address => uint256) tokenBalances; // Mapping of token addresses to their respective balances for a user
-        uint256 pointBalance; // Total points balance of the user
-        uint256 pointsPerHour; // Rate at which the user earns points per hour
-        uint256 lastUpdated; // Timestamp of the last update to user's points
+        mapping(address tokenAddress => uint256 balanceAmount) tokenBalances;
+        uint256 pointBalance;
+        uint256 pointsPerHour;
+        uint256 lastUpdated;
     }
 
-    /**
-     * @notice Deposits a specified amount of a token into the contract
-     * @param _tokenAddress The address of the token to deposit
-     * @param _amount The amount of the token to deposit
-     */
-    function deposit(address _tokenAddress, uint256 _amount) external;
+    // Custom Errors
+    error ZeroAddress();
+    error TokenNotWhitelisted();
+    error ClaimDisabled();
+    error ZeroAmount();
+    error NotClaimManager();
+    error ClaimManagerNotSet();
+    error DepositStopCannotBeRetroactive();
+    error DepositStopTimeAlreadySet();
+    error DepositStopTimeNotPassed();
+    error DepositStopTimePassed();
+    error ApprovalFailed();
+
+    // Events
 
     /**
-     * @notice Allows users to claim their points.
-     * @param _userAddress The address of the user claiming points
-     * @return pointsClaimed The total points claimed by the user
+     * Emitted when a user deposits ERC20 tokens into the contract
+     * @param from The address of the user that deposited the tokens
+     * @param tokenAddress The address of the token that was deposited
+     * @param amount The amount of the token that was deposited
      */
-    function claim(
-        address _userAddress
-    ) external returns (uint256 pointsClaimed);
+    event Deposit(
+        address indexed from,
+        address indexed tokenAddress,
+        uint256 amount
+    );
 
     /**
-     * @notice Adds a token to the list of whitelisted tokens
-     * @param _tokenAddress The address of the token to whitelist
+     * Emitted when a user claims their points via the claim contract
+     * @param user The address of the user that claimed
+     * @param pointsClaimed The number of points the user claimed
+     */
+    event ClaimPoints(address indexed user, uint256 pointsClaimed);
+
+    /**
+     * Emitted when the claim enabled state of the contract is changed
+     * @param _enabled The new state of claim enabled
+     */
+    event ClaimEnabled(bool indexed _enabled);
+
+    /**
+     * Emitted when a new token is added to the whitelist
+     * @param _tokenAddress The address of the token that was added to the whitelist
+     */
+    event WhitelistedTokenAdded(address indexed _tokenAddress);
+
+    /**
+     * Emitted when the claim manager contract is set
+     * @param _claimManager The address of the new claim manager contract
+     */
+    event ClaimManagerSet(address indexed _claimManager);
+
+    // Owner only methods
+
+    /**
+     * @dev Add a new deposit token to the contract
+     * @param _tokenAddress The address of the token to be added
      */
     function addWhitelistedToken(address _tokenAddress) external;
 
     /**
-     * @notice Enables or disables claim functionality
-     * @param _enabled Boolean indicating whether claims should be enabled
+     * @dev Set the claim enabled state of the contract
+     * @param _enabled The new state of claim enabled
      */
     function setClaimEnabled(bool _enabled) external;
 
     /**
-     * @notice Sets the address of the claim manager contract
-     * @param _claimManager The address of the claim manager
+     * @dev Set the address of the contract which is allowed to claim points on behalf of users. Can be set to the null address to disable claims.
+     * @param _newClaimManager The address of the contract which is allowed to claim points on behalf of users.
      */
-    function setClaimManager(address _claimManager) external;
+    function setClaimManager(address _newClaimManager) external;
 
     /**
-     * @notice Sets the timestamp after which points no longer accrue
-     * @param _newDepositStopTime The timestamp to set
+     * @dev Set the timestamp of the end of the points accrual period. Points will no longer accrue for any deposits beyond this timestamp.
+     * @param _newDepositStopTime The timestamp of the end of the points accrual period.
      */
     function setDepositStopTime(uint256 _newDepositStopTime) external;
 
     /**
-     * @notice Pauses the contract, disabling deposits
+     * @dev Pause the contract
      */
     function pause() external;
 
     /**
-     * @notice Unpauses the contract, enabling deposits
+     * @dev Unpause the contract
      */
     function unpause() external;
 
-    /**
-     * @notice Returns a list of all whitelisted tokens
-     * @return _allWhitelistedTokens An array of addresses of whitelisted tokens
-     */
-    function getAllWhitelistedTokens()
-        external
-        view
-        returns (address[] memory _allWhitelistedTokens);
+    // External view methods
 
     /**
-     * @notice Calculates the total points earned by a user
-     * @param _userAddress The address of the user
-     * @return currentPointsBalance The total points earned by the user
-     */
-    function calculatePoints(
-        address _userAddress
-    ) external view returns (uint256 currentPointsBalance);
-
-    /**
-     * @notice Returns the token balance of a user for a specific token
-     * @param _userAddress The address of the user
-     * @param _tokenAddress The address of the token
-     * @return balance The token balance of the user
-     */
-    function tokenBalance(
-        address _userAddress,
-        address _tokenAddress
-    ) external view returns (uint256 balance);
-
-    /**
-     * @notice Returns detailed user information including points per hour, last updated timestamp, and point balance
-     * @param _userAddress The address of the user
-     * @return pointsPerHour The rate at which the user earns points per hour
-     * @return lastUpdated The timestamp of the last update to user's points
-     * @return pointBalance The total points balance of the user
+     * @notice A view method that returns point information about the provided address
+     * @param _userAddress The address of the user to check the point information for.
+     * @return pointsPerHour The number of points earned per hour by the user.
+     * @return lastUpdated The timestamp of the last time the user's points were updated.
+     * @return pointBalance The total number of points earned by the user.
      */
     function getUserInfo(
         address _userAddress
@@ -115,41 +127,50 @@ interface IOmronDeposit {
             uint256 pointBalance
         );
 
-    // Events
     /**
-     * @dev Emitted when a user deposits ERC20 tokens into the contract
-     * @param from The address of the user that deposited the tokens
-     * @param tokenAddress The address of the token that was deposited
-     * @param amount The amount of the token that was deposited
+     * @notice A view method that returns the list of all whitelisted tokens.
+     * @return _allWhitelistedTokens An array of addresses of all whitelisted tokens.
      */
-    event Deposit(
-        address indexed from,
-        address indexed tokenAddress,
-        uint256 amount
-    );
+    function getAllWhitelistedTokens()
+        external
+        view
+        returns (address[] memory _allWhitelistedTokens);
 
     /**
-     * @dev Emitted when a user claims points
-     * @param user The address of the user that claimed
-     * @param pointsClaimed The number of points claimed by the user
+     * @notice A view method that calculates the points earned by a user.
+     * @param _userAddress The address of the user to calculate the points for.
+     * @return currentPointsBalance The total points earned by the user, including points earned from time elapsed since the last update.
      */
-    event Claim(address indexed user, uint256 pointsClaimed);
+    function calculatePoints(
+        address _userAddress
+    ) external view returns (uint256 currentPointsBalance);
 
     /**
-     * @dev Emitted when the claim enabled state is changed
-     * @param _enabled The new state of claim enabled
+     * @notice A view method that returns the token balance for a user.
+     * @param _userAddress The address of the user to check the token balance for.
+     * @param _tokenAddress The address of the token to check the balance for.
+     * @return balance The token balance of the user for the specified token.
      */
-    event ClaimEnabled(bool indexed _enabled);
+    function tokenBalance(
+        address _userAddress,
+        address _tokenAddress
+    ) external view returns (uint256 balance);
+
+    // External methods
 
     /**
-     * @dev Emitted when a new token is added to the whitelist
-     * @param _tokenAddress The address of the token that was whitelisted
+     * @dev Deposit a token into the contract
+     * @param _tokenAddress The address of the token to be deposited
+     * @param _amount The amount of the token to be deposited
      */
-    event WhitelistedTokenAdded(address indexed _tokenAddress);
+    function deposit(address _tokenAddress, uint256 _amount) external;
 
     /**
-     * @dev Emitted when the claim manager is set
-     * @param _claimManager The address of the claim manager
+     * @dev Called by the claim manager to claim all points for the user
+     * @param _userAddress The address of the user to claim for
+     * @return pointsClaimed The number of points claimed by the user
      */
-    event ClaimManagerSet(address indexed _claimManager);
+    function claim(
+        address _userAddress
+    ) external returns (uint256 pointsClaimed);
 }
